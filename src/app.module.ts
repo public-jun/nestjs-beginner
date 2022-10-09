1dee44f8d05a8a935d678b250c5f686840859f4c
public-jun <generalpublicjunjun@gmail.com>
2022-10-09 17:50:37 +0900

    Add: Create Item table

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { ItemsModule } from './items/items.module';
+import { AppDataSource } from './app.datasource';
 
 @Module({
-  imports: [ItemsModule],
+  imports: [ItemsModule, TypeOrmModule.forRoot(AppDataSource.options)],
   controllers: [],
   providers: [],
 })
